49a49d7a285d47bee21cd4ce1b2cba7e92baa111
Pavel Sasnouski <sosnovskiy.paul@gmail.com>
2023-01-13 23:45:33 +0300

    add task1 to task12

--- src/es-features/task10.js
+++ src/es-features/task10.js
@@ -53,5 +53,44 @@ export function task10Old() {
 // Названия классов должны быть NewA и NewB
 // Изменить тело функции task10Old, написав task10New, где заиспользуете
 // класс так, как использовалась функция-конструктор B в task10Old
+class NewA {
+  constructor(name) {
+    this.name = name;
+  }
+}
+class NewB extends NewA {
+  constructor(name, age) {
+    super(name);
+    this.age = age;
+  }
 
-export function task10New() {}
+  getName(text) {
+    return `${this.name} ${text}`;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  static defaultUser() {
+    return new NewB('text', 0);
+  }
+
+  getColor() {
+    return this._color;
+  }
+
+  setColor(value) {
+    this._color = value;
+  }
+}
+
+export const task10New = () => {
+  const b = new NewB('Max', 12);
+  console.log(b.getName('Best')); // Max Best
+  console.log(b.getAge()); // 12
+  console.log(NewB.defaultUser()); // {name: 'test', age: 0}
+  b.setColor('red');
+  console.log(b.getColor()); // red
+  return b;
+};
